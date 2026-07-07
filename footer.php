0faa5d9f0c2faa6481f0217724bd0af92dabb3a6
Dauren <daurenswrld@gmail.com>
2026-07-07 17:13:45 +0500

    Update PWA modal delay to 3000ms

--- footer.php
+++ footer.php
@@ -311,7 +311,7 @@
         if (!isStandalone && !isDismissed) {
             setTimeout(function() {
                 overlay.classList.add('is-open');
-            }, 100);
+            }, 3000);
         }
     });
     </script>
